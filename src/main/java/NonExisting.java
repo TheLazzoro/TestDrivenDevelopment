27a823f9d1d7cc89981802aef3ccdc2116719834
TheLazzoro <43842641+TheLazzoro@users.noreply.github.com>
2022-01-31 20:07:16 +0100

    Initial commit

---
+++ src/main/java/NonExisting.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lasse Dam
+ */
+public class NonExisting {
+
+    public String nonExistingMethod() {
+        return "Hello";
+    }
+
+}
---
+++ src/test/java/JUnitTest.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Lasse Dam
+ */
+public class JUnitTest {
+
+    public JUnitTest() {
+    }
+
+    @Test
+    public void testNonExisting() {
+        NonExisting ne = new NonExisting(); //alt+enter = context menu with create Class.
+        String actual = ne.nonExistingMethod();
+        assertEquals("Hello", actual);
+    }
+    
+    @BeforeAll
+
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
